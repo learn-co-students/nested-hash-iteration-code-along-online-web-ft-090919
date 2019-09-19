39851aad07657faa2b7d1ef6133d4c25d162acd5
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-19 05:39:04 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,14 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person , details|
+  details.each do |key, value| 
+  if key == :favorite_ice_cream_flavors
+    value.delete_if do |flavour|
+      flavour == "strawberry"
+    end
 end
+end
+end 
+end
+
